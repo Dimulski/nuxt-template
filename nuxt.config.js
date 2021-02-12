2052193bd356e68918e8d5e6de77a356c326df80
Georgi Dimulski <george.dimulski@gmail.com>
2021-02-12 15:43:18 +0200

    Update dependancies and assets with bootstrap-vue-argon-dashboard

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,10 +21,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    // '~/plugins/argon-dashboard.js'
+    '~/plugins/argon-dashboard.js',
     '~/components/SidebarPlugin/index.js',
     '~/directives/click-outside.js'
-
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
